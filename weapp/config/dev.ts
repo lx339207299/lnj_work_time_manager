a306f13e46e3c2c73ca2171ac7a9f494ad1e3485
lxxx <339207200@qq.com>
2026-02-27 21:52:56 +0800

    恢复显示请求test-yggl

--- weapp/config/dev.ts
+++ weapp/config/dev.ts
@@ -1,8 +1,8 @@
 module.exports = {
   env: {
     NODE_ENV: '"development"',
-    // TARO_APP_API_URL: '"https://test-yggl.bear0811.cn/api"'
-    TARO_APP_API_URL: '"http://192.168.1.122:3000/api"'
+    TARO_APP_API_URL: '"https://test-yggl.bear0811.cn/api"'
+    // TARO_APP_API_URL: '"http://192.168.1.122:3000/api"'
   },
   defineConstants: {
   },
